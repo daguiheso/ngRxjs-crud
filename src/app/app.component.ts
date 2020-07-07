d95f4f3b48a29ac971f791c5ee8bd256d968de37
Daniel H <daguiheso@gmail.com>
2020-07-07 01:47:15 -0500

    select customers from store in appComponet

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,18 @@
 import { Component } from '@angular/core';
+import { Store, select } from '@ngrx/store';
+import { AppState } from './store';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
+
 export class AppComponent {
-  title = 'users-crud';
+
+  constructor(private store: Store<AppState>) {
+    store.pipe(select('customers')).subscribe(res => {
+      console.log(res);
+    });
+  }
 }
